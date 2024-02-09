7dc3ad6e91d4235fbe1c6c355de43f588f9c06d3
Tich Z <tzvidzie@gmail.com>
2024-02-09 03:53:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TextToSpeech from './Components/TextToSpeech';
+import './App.css'; 
 
-function App() {
+const BlogPost = () => {
+  const text =`Hello Tich`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Blog Post</h1>
+      <TextToSpeech text={text} />
+      <p>{text}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default BlogPost;
